d7f44bb6b8d13d745d3c82e6ad6497f47a1a5201
DEB108-Nitai <devranjanpramanick@gmail.com>
2026-01-31 13:56:41 +0000

    DataTypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,33 @@
+"use strict" // treat all js code as newer version 
+
+// alert(3+3) // we are using nodejs , not browser 
+
+// code readability should be high 
+console.log(3+3)
+
+console.log("Nityananda")
+
+let name = "NitaiChand"
+let age = 16
+let isLoggedin = true 
+
+// Data Types 
+// Numbers => till 2 ^ 53
+//bigint => if we have to deal with big numbers 
+// string => ""
+// boolean => true/false 
+// null => standalone value 
+// undefined 
+// symbol => unique
+
+// object 
+
+console.log(typeof name ) // prints string 
+
+console.log(typeof age ) // prints number 
+
+console.log(typeof isLoggedin) //prints boolean 
+
+console.log(typeof undefined) // prints undefined 
+
+console.log(typeof null ) // prints object
