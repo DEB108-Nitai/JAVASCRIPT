a9b06815dd372f4efb3638fec7be762008868546
DEB108-Nitai <devranjanpramanick@gmail.com>
2026-02-03 16:57:42 +0000

    comparison

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// comparison b/w diff data types 
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+// == and inequality works differently in js . inequality converts null into number so outputs come accordingly but == doesn't do the same 
+
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===   : strict checking , it also checks the data type  
+
+console.log("2" === 2);
